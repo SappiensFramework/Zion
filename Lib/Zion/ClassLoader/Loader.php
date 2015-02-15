676cfaba6cf1684b0e0e81e1651fc6ad8e408c96
jfranciscos4 <silvaivctd@gmail.com>
2015-02-15 19:38:54 -0300

    Remover o autoloader

--- Lib/Zion/ClassLoader/Loader.php
+++ Lib/Zion/ClassLoader/Loader.php
@@ -39,45 +39,45 @@
  * 
  */
 
-namespace Zion\ClassLoader;
+// namespace Zion\ClassLoader;
 
-class Loader
-{
-    private $nameSpaces;
+// class Loader
+// {
+//     private $nameSpaces;
 
-    public function __construct()
-    {
-        $this->nameSpaces = [];
-    }
+//     public function __construct()
+//     {
+//         $this->nameSpaces = [];
+//     }
 
-    /**
-     * Registra os namespaces setados em $nameSpaces
-     * @return object
-     */
-    public function inicio()
-    {
-        require_once "UniversalClassLoader.php";
+//     /**
+//      * Registra os namespaces setados em $nameSpaces
+//      * @return object
+//      */
+//     public function inicio()
+//     {
+//         require_once "UniversalClassLoader.php";
 
-        $loader = new \Zion\ClassLoader\UniversalClassLoader();
+//         $loader = new \Zion\ClassLoader\UniversalClassLoader();
 
-        foreach ($this->nameSpaces as $name => $caminho) {
+//         foreach ($this->nameSpaces as $name => $caminho) {
 
-            $loader->registerNamespace($name, $caminho);
-        }
+//             $loader->registerNamespace($name, $caminho);
+//         }
 
-        $loader->register();
-    }
+//         $loader->register();
+//     }
 
-    /**
-     * Seta nome e caminho para um namespace
-     * @param string $name
-     * @param string $caminho
-     * @return \Zion\ClassLoader\Loader
-     */
-    public function setNameSpaces($name, $caminho)
-    {
-        $this->nameSpaces[$name] = $caminho;
-        return $this;
-    }
+//     /**
+//      * Seta nome e caminho para um namespace
+//      * @param string $name
+//      * @param string $caminho
+//      * @return \Zion\ClassLoader\Loader
+//      */
+//     public function setNameSpaces($name, $caminho)
+//     {
+//         $this->nameSpaces[$name] = $caminho;
+//         return $this;
+//     }
 
-}
+// }
