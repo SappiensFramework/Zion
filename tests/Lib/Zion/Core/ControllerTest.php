3c6210cc071f9b08c32e79dd96bc4d613bb7d35d
José Francisco da Silva IV <silvaivctd@gmail.com>
2015-02-16 13:49:17 +0000

    Adicionando o primeiro teste

---
+++ tests/Lib/Zion/Core/ControllerTest.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ *
+ *    Sappiens Framework
+ *    Copyright (C) 2014, BRA Consultoria
+ *
+ *    Website do autor: www.braconsultoria.com.br/sappiens
+ *    Email do autor: sappiens@braconsultoria.com.br
+ *
+ *    Website do projeto, equipe e documentação: www.sappiens.com.br
+ *
+ *    Este programa é software livre; você pode redistribuí-lo e/ou
+ *    modificá-lo sob os termos da Licença Pública Geral GNU, conforme
+ *    publicada pela Free Software Foundation, versão 2.
+ *
+ *    Este programa é distribuído na expectativa de ser útil, mas SEM
+ *    QUALQUER GARANTIA; sem mesmo a garantia implícita de
+ *    COMERCIALIZAÇÃO ou de ADEQUAÇÃO A QUALQUER PROPÓSITO EM
+ *    PARTICULAR. Consulte a Licença Pública Geral GNU para obter mais
+ *    detalhes.
+ *
+ *    Você deve ter recebido uma cópia da Licença Pública Geral GNU
+ *    junto com este programa; se não, escreva para a Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *    02111-1307, USA.
+ *
+ *    Cópias da licença disponíveis em /Sappiens/_doc/licenca
+ *
+ */
+/**
+ * Controller()
+ * @author Jose Francisco da Silva IV <silvaivctd@gmail.com>
+ * @since 14/02/2015
+ * @version 1.0
+ * @copyright 2015
+ *
+ * Controller
+ */
+require __DIR__ . '/../../../../vendor/autoload.php';
+
+use \Zion\Core\Controller;
+
+class ControllerTest extends PHPUnit_Framework_TestCase
+{	
+	private $instance;
+	
+	public function setUp()
+	{
+		$this->instance  = new \Zion\Core\Controller();
+		parent::setUp();
+	}
+	
+	public function testInstanceOf()
+	{
+		$this->assertInstanceOf("Zion\Core\Controller", $this->instance);
+	}
+	/**
+	 * 
+	 */
+	public function testControle()
+	{
+		$test = 'teste';
+		$this->instance->controle($test);
+		$this->assertEquals($test, $this->instance->getAcao());		
+	}
+	
+}
